804213ac87dff911bc01f38b0c1c9cdba4d4d1db
nekolr <excalibll@163.com>
2018-11-14 17:27:27 +0800

    Update version

--- src/main/java/com/nekolr/view/Main.java
+++ src/main/java/com/nekolr/view/Main.java
@@ -17,7 +17,7 @@ import java.io.IOException;
  */
 public class Main extends Application {
 
-    private static final String TITLE = "Sirius 增量包生成工具 V0.2";
+    private static final String TITLE = "Sirius 增量包生成工具 V0.3";
 
     @Override
     public void start(Stage primaryStage) {
